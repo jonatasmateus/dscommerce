8ba5c65054f185eecef7a55e9fc69ecfd2601536
Jonatas <jonatasmateuscontato@gmail.com>
2024-01-02 18:08:43 -0300

    GET products paged

--- src/main/java/com/jonatasmateus/dscommerce/controllers/ProductController.java
+++ src/main/java/com/jonatasmateus/dscommerce/controllers/ProductController.java
@@ -3,10 +3,11 @@ package com.jonatasmateus.dscommerce.controllers;
 import com.jonatasmateus.dscommerce.dto.ProductDTO;
 import com.jonatasmateus.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -14,6 +15,11 @@ public class ProductController {
 
     @Autowired
     private ProductService service;
+
+    @GetMapping()
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
+    }
 
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
--- src/main/java/com/jonatasmateus/dscommerce/services/ProductService.java
+++ src/main/java/com/jonatasmateus/dscommerce/services/ProductService.java
@@ -4,16 +4,24 @@ import com.jonatasmateus.dscommerce.dto.ProductDTO;
 import com.jonatasmateus.dscommerce.entities.Product;
 import com.jonatasmateus.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class ProductService {
 
     @Autowired
     private ProductRepository repository;
+
+    @Transactional
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(ProductDTO::new);
+    }
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
