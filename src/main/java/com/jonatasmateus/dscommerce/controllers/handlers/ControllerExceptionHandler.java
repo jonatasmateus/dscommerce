e5d7a8f30570535e8fa55939cb48c23bcdb33178
Jonatas <jonatasmateuscontato@gmail.com>
2024-01-05 08:25:08 -0300

    Exception handling implemented

---
+++ src/main/java/com/jonatasmateus/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.jonatasmateus.dscommerce.controllers.handlers;
+
+import com.jonatasmateus.dscommerce.dto.CustomError;
+import com.jonatasmateus.dscommerce.services.exceptions.DatabaseException;
+import com.jonatasmateus.dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/jonatasmateus/dscommerce/services/ProductService.java
+++ src/main/java/com/jonatasmateus/dscommerce/services/ProductService.java
@@ -3,10 +3,15 @@ package com.jonatasmateus.dscommerce.services;
 import com.jonatasmateus.dscommerce.dto.ProductDTO;
 import com.jonatasmateus.dscommerce.entities.Product;
 import com.jonatasmateus.dscommerce.repositories.ProductRepository;
+import com.jonatasmateus.dscommerce.services.exceptions.DatabaseException;
+import com.jonatasmateus.dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -23,7 +28,8 @@ public class ProductService {
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-        Product product = repository.findById(id).get();
+        Product product = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado"));
         return new ProductDTO(product);
     }
 
@@ -36,14 +42,27 @@ public class ProductService {
 
     @Transactional
     public ProductDTO update(Long id, ProductDTO dto) {
-        Product entity = repository.getReferenceById(id);
-        passDtoToEntity(dto, entity);
-        return new ProductDTO(repository.save(entity));
+        try {
+            Product entity = repository.getReferenceById(id);
+            passDtoToEntity(dto, entity);
+            return new ProductDTO(repository.save(entity));
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(Long id) {
-        repository.deleteById(id);
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
     }
 
     private void passDtoToEntity(ProductDTO dto, Product entity) {
