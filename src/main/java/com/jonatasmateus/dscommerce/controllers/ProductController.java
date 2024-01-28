634f01dd36667603fa0389030c1110d54c8fac38
Jonatas <jonatasmateuscontato@gmail.com>
2024-01-28 20:25:33 -0300

    Products query by name implemented

--- src/main/java/com/jonatasmateus/dscommerce/controllers/ProductController.java
+++ src/main/java/com/jonatasmateus/dscommerce/controllers/ProductController.java
@@ -20,8 +20,10 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
-        Page<ProductDTO> dto = service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(
+            @RequestParam(name = "name", defaultValue = "") String name,
+            Pageable pageable) {
+        Page<ProductDTO> dto = service.findAll(name, pageable);
         return ResponseEntity.ok(dto);
     }
 
